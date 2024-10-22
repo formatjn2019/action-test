e5dd1b123bcf639f81b4d71324d3a1fc545dd89a
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2024-10-22 21:58:36 +0800

    add test file

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     print(git)
     print(23423)
     with open("./result.txt","w") as f:
-        f.write("fadfdaffffffffffffffffffp[fff")
+        f.write("fadfdaffffffffffs2222222222222ffffffffp[fff")
